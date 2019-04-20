895508f29a867b8b03bf41edd870105e7fb94f27
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 11:23:39 +0900

    BMI 계산, 비만 판정까지 마무리

--- app/src/main/java/com/tj/bmicalculator/ResultActivity.java
+++ app/src/main/java/com/tj/bmicalculator/ResultActivity.java
@@ -24,6 +24,37 @@ public class ResultActivity extends AppCompatActivity {
 
     void setValues() {
 
+//        메인액티비티가 던져준 키/몸무게 받기.
+
+        double height = getIntent().getDoubleExtra("키", 0);
+        double weight = getIntent().getDoubleExtra("몸무게", 0);
+
+//        BMI 값을 계산.
+
+        double BMI = weight / ((height / 100) * (height / 100));
+
+//        계산된 BMI값을 출력
+
+        bmiCalculateTxt.setText(String.format("%f 입니다.", BMI));
+
+//        계산된 BMI값을 토대로 비만 여부 판정
+        if (BMI < 18.5) {
+            bmiResultTxt.setText("저체중");
+        }
+        else if (BMI < 23) {
+            bmiResultTxt.setText("정상");
+        }
+        else if (BMI < 25) {
+            bmiResultTxt.setText("과체중");
+        }
+        else if (BMI < 30) {
+            bmiResultTxt.setText("비만");
+        }
+        else {
+            bmiResultTxt.setText("고도비만");
+        }
+
+
     }
 
     void bindViews() {
