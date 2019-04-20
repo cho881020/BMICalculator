406f58e025db20564e1690eab6887c458155bc3e
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 11:02:55 +0900

    결과액티비티 JAVA XML 연동

--- app/src/main/java/com/tj/bmicalculator/ResultActivity.java
+++ app/src/main/java/com/tj/bmicalculator/ResultActivity.java
@@ -1,13 +1,34 @@
 package com.tj.bmicalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private android.widget.TextView bmiResultTxt;
+    private android.widget.TextView bmiCalculateTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    void setupEvents() {
+
+    }
+
+    void setValues() {
+
+    }
+
+    void bindViews() {
+
+        this.bmiCalculateTxt = (TextView) findViewById(R.id.bmiCalculateTxt);
+        this.bmiResultTxt = (TextView) findViewById(R.id.bmiResultTxt);
     }
 }
