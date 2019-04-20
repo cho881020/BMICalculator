3ebde8e62f4cea60bc7b6ce510765b5962c247ad
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 11:13:35 +0900

    입력값 추출해서 ResultActivity로 전송

--- app/src/main/java/com/tj/bmicalculator/MainActivity.java
+++ app/src/main/java/com/tj/bmicalculator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tj.bmicalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+//                확인버튼을 누르면? => ResultActivity로 키/몸무게를 들고 이동
+
+//                1) 입력된 키가 몇인지 소수점으로 저장
+//                1.1) heightEdt에 입력된 값을 일단 String으로 뽑아오자.
+                String heightStr = heightEdt.getText().toString();
+
+//                1.2) 따낸 String을 double 형태로 변환.
+
+                double height = Double.parseDouble(heightStr);
+
+//                1.3) 몸무게도 키와 같은 방식으로 소수점 추출
+                String weightStr = weightEdt.getText().toString();
+                double weight = Double.parseDouble(weightStr);
+
+//                2) 인텐트를 만들고, 따낸 키와 몸무게를 첨부.
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("키", height);
+                intent.putExtra("몸무게", weight);
+                startActivity(intent);
 
             }
         });
